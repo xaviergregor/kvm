aef24ca14b73b3731c06e400d96fb23f99366741
xaviergregor <xavier@gregor.fr>
2021-11-24 08:17:35 +0100

    files for install new VM

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+# Let's Go !! #################################################
+
+
+virt-install \
+	--name debian \
+	--vcpus=2 \
+	--ram=4096 \
+	--location="http://ftp.fr.debian.org/debian/dists/Debian11.1/main/installer-amd64/" \
+	--initrd-inject=preseed.cfg \
+	--extra-args="auto console=ttyS0,115200n8 serial" \
+	--graphics none \
+	--console pty,target_type=serial \
+	--extra-args="ks=file:/preseed.cfg" \
+	--network default,model=virtio \
+	--os-type=linux \
+	--os-variant=debiantesting \
+	--disk=pool=default,size=20,format=qcow2,bus=virtio
